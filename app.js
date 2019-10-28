00d145a2814e10fb1319f1628142a5fb2f110781
busroutemap <kotokotaro@gmail.com>
2019-10-28 15:59:45 +0900

    devRouter定義

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// devRouter定義
+var devRouter = require('./routes/dev');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// devRouter追加
+app.use('/dev', devRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
