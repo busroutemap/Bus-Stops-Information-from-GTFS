5a1a8ff5ae220f68d869d321a32c6ba7ebb70dbb
busroutemap <kotokotaro@gmail.com>
2019-10-30 14:08:22 +0900

    userを一時的にコメントアウト

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 // devRouter定義
 var devRouter = require('./routes/dev');
 
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 // devRouter追加
 app.use('/dev', devRouter);
 
