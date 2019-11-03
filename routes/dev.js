8dc3ad32c43bc9201d5147c5c7452cbc06ec68d6
busroutemap <kotokotaro@gmail.com>
2019-11-03 22:29:40 +0900

    async-awaitで大幅改善

--- routes/dev.js
+++ routes/dev.js
@@ -1,63 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var getGTFS = require('./mymodule/getGTFS')
-var appSettings = require('./mymodule/appSettings')
+var getGTFS = require('./mymodule/getGTFS');
+// var periods = require('./mymodule/makePeriodList');
+// var fareLists = require('./mymodule/makeFareList');
+// var appSettings = require('./mymodule/appSettings');
 //---------------------------------------------
-// nodeGTFS周りの設定
-const gtfs = require('gtfs');
-const mongoose = require('mongoose');
-const config = {
-    mongoUrl: 'mongodb://localhost:27017/gtfs',
-};
-// 入力に応じて処理を分ける
-// user、app、gtfs
-
-
-//---------------------------------------------
-let api_user = function(req, res, next){
+let api = async function(req, res, next){
     mongoose.connect(config.mongoUrl, {useNewUrlParser: true});
     // http://localhost:3000/dev/?stop_id="id指定"
     // http://localhost:3000/dev/?stop_id=S00525AGC9070001018357H001
     // 敷島公園北
     const stop_id = req.query.stop_id;
-    let pages = {};
-    //---------------------------------------------
-    selectedStop(stop_id)
-    .then(stop => {
-        let routes = {}
-        routes = getRoutes(stop);
-        return stop, routes
-    })
-    .then((stop, routes) => {
-        let stops = {};
-        routes.forEach(element => {
-            stops[element.route_id] = getEachStops(element);
-        });
-        let fareRules = {};
-        routes.forEach(element => {
-            fareRules[element.route_id] = getEachFareRules(element, stop);
-        });
-        let stopTimes = {}
-        routes.forEach(element => {
-            stopTimes[element.route_id] = getEach1stTrip(element);
-        })
-        return stop, routes, stops, fareRules, stopTimes;
-    })
-    .then((stop, routes, stops, fareRules, stopTimes) => {
-        pages = {stop, routes, stops, fareRules, stopTimes};
-        return pages
-        // return stop, routes, stops, fareRules, stopTimes;
-    })
-    .then(pages => {
-        res.header('Content-Type', 'application/json; charset=utf-8')
-        res.send(pages);
-    })
-    return
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    let data = await getGTFS.getData(stop_id)
+    res.send(data);
 }
-
 //---------------------------------------------
 // apis配列に記述していく順に各ミドルウェアが実行
-let apis = [api_user]
+let apis = [api]
 // http://localhost:3000/dev/
 router.get('/', apis);
 
--- routes/mymodule/getGTFS.js
+++ routes/mymodule/getGTFS.js
@@ -4,98 +4,95 @@ const config = {
     mongoUrl: 'mongodb://localhost:27017/gtfs',
 };
 
-
-
-let selectedStop = function(stop_id){
-    const p = new Promise((resolve, reject) => {
-        gtfs.getStops({
+let getData = async (stop_id) => {
+    //---------------------------------------------
+    let p1 = (stop_id) => {
+        const p = new Promise((resolve, reject) => {
+            gtfs.getRoutes({
+                stop_id : stop_id
+            }, {
+                route_id : 1,
+                agency_id : 1,
+                route_short_name : 1,
+                route_long_name: 1,
+                jp_parent_route_id: 1,
+                agency_key: 1
+            })
+            .then(routes => resolve(routes))
+        });
+        return p
+    }
+    let routes = await p1(stop_id);
+    //---------------------------------------------
+    let p2getEachStops = (routes) => {
+        const p = new Promise((resolve, reject) => {
+            gtfs.getStops({
+                route_id : routes.route_id
+            },{
+                stop_id : 1,
+                stop_code : 1,
+                stop_name : 1,
+                stop_desc : 1,
+                zone_id : 1,
+                location_type : 1,
+                parent_station : 1
+            })
+            .then(stops => resolve(stops))
+        });
+        return p;
+    }
+    let eachStops = p2getEachStops(routes)
+    //---------------------------------------------
+    let p2getEachFareRules = (routes, stop_id) => {
+        let origin_id = await gtfs.getStops({
+            route_id : routes.route_id,
             stop_id : stop_id
         },{
-            stop_id : 1,
-            stop_code : 1,
-            stop_name : 1,
-            stop_desc : 1,
-            zone_id : 1,
-            location_type : 1,
-            parent_station : 1
-        })
-        .then(stop=>resolve(stop))
-    });
-    return p;
-}
-
-
-let getRoutes = function(reqStop) {
-    const p = new Promise((resolve, reject) => {
-        gtfs.getRoutes({
-            stop_id : reqStop.stop_id
-        },{
-            route_id : 1,
-            agency_id : 1,
-            route_short_name : 1,
-            route_long_name: 1,
-            jp_parent_route_id: 1,
-            agency_key: 1
-        })
-        .then(routes=>resolve(routes))
-    });
-    return p;
-}
-
-let getEachStops = function(reqRoute) {
-    const p = new Promise((resolve, reject) => {
-        gtfs.getStops({
-            route_id : reqRoute.route_id
-        },{
-            stop_id : 1,
-            stop_code : 1,
-            stop_name : 1,
-            stop_desc : 1,
-            zone_id : 1,
-            location_type : 1,
-            parent_station : 1
-        })
-        .then(stops => resolve(stops))
-    });
-    return p;
-}
-let getEachFareRules = function(reqRoute, reqStop){
-    const p = new Promise((resolve, reject) => {
-        gtfs.getFareRules({
-            route_id : reqRoute.route_id,
-            origin_id : stop.zone_id
-        },{
-            // fareRulesの配列が何かにもよる
-            // fareattributionsが含まれてい無いと思うけど、
-            // そうなれば、mongooseからfare_idを問い合わせる必要がある
-        })
-        .then(rules => resolve(rules))
-    });
-    return p;
-}
-
-let getEach1stTrip = function(reqRoute){
-    const p = new Promise((resolve, reject) => {
-        gtfs.getTrips({
-            route_id : reqRoute.route_id,
-        },{
-            trip_id : 1,
-            service_id : 1
-        })
-        .then(trips => {
-            gtfs.getStoptimes({
-                trip_id : trips[0],
-                route_id : reqRoute.route_id
+            zone_id : 1
+        });
+        const p = new Promise((resolve, reject) => {
+            gtfs.getFareRules({
+                route_id : routes.route_id,
+                origin_id : origin_id
             },{
-                arrival_time : 1,
-                departure_time : 1,
-                stop_id : 1,
-                stop_sequence : 1,
-                stop_headsign : 1
+                // fareRulesの配列が何かにもよる
+                // fareattributionsが含まれてい無いと思うけど、
+                // そうなれば、mongooseからfare_idを問い合わせる必要がある
             })
+            .then(rules => resolve(rules))
+        });
+        return p;
+    }
+    let ruleLists = p2getEachFareRules(routes, stop_id);
+    //---------------------------------------------
+    let p2getEach1stTrip = function(routes){
+        const p = new Promise((resolve, reject) => {
+            gtfs.getTrips({
+                route_id : routes.route_id,
+            },{
+                trip_id : 1,
+                service_id : 1
+            })
+            .then(trips => {
+                gtfs.getStoptimes({
+                    trip_id : trips[0],
+                    route_id : reqRoute.route_id
+                },{
+                    arrival_time : 1,
+                    departure_time : 1,
+                    stop_id : 1,
+                    stop_sequence : 1,
+                    stop_headsign : 1
+                })
+            })
+            .then(stop_times => resolve(stop_times))
         })
-        .then(stop_times => resolve(stop_times))
-    })
-    return p
+        return p
+    }
+    let periodLists = p2getEach1stTrip(routes);
+    //---------------------------------------------
+    return routes,eachStops,ruleLists,periodLists
 }
+
+module.exports = getData;
 
