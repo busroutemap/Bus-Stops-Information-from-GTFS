5637bf27eeb94b9aae49c7cd97ca8cf4d6cf9597
busroutemap <kotokotaro@gmail.com>
2020-02-07 00:46:28 +0900

    試行錯誤中、エラー吐くからV2動かない

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const getData = require('./mymodule/getData');
+const getDataV2 = require('./v2/getData');
 const mongoose = require('mongoose');
 // mongooseがグローバルインストールになっている
 // ここを変えれば？？？
@@ -20,7 +21,8 @@ const api = async function(req, res, next){
     // 三中前 S00517AGC9070001018357H001
     // 本町 S00436AGC9070001018357H001
     const stop_id = req.query.stop_id;
-    let data = await getData(stop_id);
+    // let data = await getData(stop_id);
+    let data = getDataV2(stop_id);
     res.header('Content-Type', 'application/json; charset=utf-8');
     res.send(data);
 }
